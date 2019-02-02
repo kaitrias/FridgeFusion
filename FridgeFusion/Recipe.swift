c577cc08a4820f5b35d9ef6077e79221c63cf6f1
Jasmine Dirksen <jasmineorjasnice@gmail.com>
2019-02-02 15:48:56 -0800

    recipe tables

---
+++ FridgeFusion/Recipe.swift
@@ -0,0 +1,24 @@
+//
+//  Recipe.swift
+//  FridgeFusion
+//
+//  Created by Jasmine Dirksen on 2/2/19.
+//  Copyright © 2019 Kaito Trias. All rights reserved.
+//
+
+import UIKit
+
+class Recipe: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
