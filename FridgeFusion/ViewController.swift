0486c80bb5bc5f0a3316ffd6a479bd4e830289ee
kaitrias <kaitrias@gmail.com>
2019-02-03 02:51:33 -0800

    Remove redundant code

--- FridgeFusion/ViewController.swift
+++ FridgeFusion/ViewController.swift
@@ -53,54 +53,6 @@ class ViewController: UITableViewController,
         presentCaptureViewController()
     }
     
-    private func setupCaptureSession() {
-        let captureSession = AVCaptureSession()
-        
-        
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
-        captureSession.addInput(input)
-        
-        captureSession.startRunning()
-        
-        
-        //add camera to view
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
-        
-        let dataOutput = AVCaptureVideoDataOutput()
-        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
-        captureSession.addOutput(dataOutput)
-        
-    }
-    
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        
-        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        
-        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
-        
-        let request = VNCoreMLRequest(model: model) {
-            (finishedReq, err) in
-            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
-            if let item = results.first(where: {$0.identifier == "banana"}) {
-                if item.confidence > 0.80 {
-                    print(item.identifier, item.confidence)
-                    let message: String = item.identifier + "found?"
-                    let alert = UIAlertController(title: message, message: nil, preferredStyle: UIAlertController.Style.alert)
-                    
-                    alert.addAction(UIAlertAction(title: "Yes", style: UIAlertAction.Style.default, handler: nil))
-                    alert.addAction(UIAlertAction(title: "No", style: UIAlertAction.Style.cancel, handler: nil))
-                    
-                    self.present(alert, animated: true, completion: nil)
-                }
-            }
-        }
-        
-        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
-    }
-    
     private func setupActionSheet() {
     }
 
